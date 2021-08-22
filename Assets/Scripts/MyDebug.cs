06c30a903e7a79e0b41cb152d3590a476bdc84d0
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-08-22 14:49:08 +0700

    My Debug - Added print functions.

---
+++ Assets/Scripts/MyDebug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDebug : MonoBehaviour
+{
+    public void _Print(string _text)
+    {
+        Debug.Log(_text);
+    }
+
+    public void _PrintWarn(string _text)
+    {
+        Debug.LogWarning(_text);
+    }
+
+    public void _PrintError(string _text)
+    {
+        Debug.LogError(_text);
+    }
+}
