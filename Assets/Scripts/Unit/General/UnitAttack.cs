be35c087ed152ac31d7e443dbe807555b2d89afd
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-08-22 18:16:57 +0700

    Unit attack - Added attack interval.

--- Assets/Scripts/Unit/General/UnitAttack.cs
+++ Assets/Scripts/Unit/General/UnitAttack.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UnitAttack : MonoBehaviour
 {
-    [SerializeField] float _attackInterval = 1f;
+    [SerializeField] UnitAreaToAttack _areaToAttack;
+
+    [SerializeField] float _attackInterval = 20f;
+    [SerializeField] float _damagePerHit = 20f;
     bool _isAttackOnCooldown = false;
 
-    UnitAreaToAttack _areaToAttack;
-
-    void Awake()
-    {
-        _areaToAttack = GetComponent<UnitAreaToAttack>();
-    }
+    [Header("Events")]
+    [SerializeField] UnityEvent _onStartAttack;
 
     void Update()
     {
-        
+        _checkTargetToAttack();
+    }
+
+    void _checkTargetToAttack()
+    {
+        if(_areaToAttack._unitWithinArea.Count > 0)
+        {
+            _attackToUnit(_areaToAttack._unitWithinArea[0]);
+        }
+    }
+
+    void _attackToUnit(Unit _targetUnit)
+    {
+        if(_targetUnit.gameObject.active == true
+            && !_isAttackOnCooldown)
+        {
+            _targetUnit.GetComponent<UnitHealth>()._TakeDamage(_damagePerHit);
+            Debug.Log($"{this.name} attack to {_targetUnit.name}");
+
+            _onStartAttack.Invoke();
+
+            StartCoroutine(_countAttackInterval(_attackInterval));
+        }
+    }
+
+    public IEnumerator _countAttackInterval(float _cooldownTime)
+    {
+        _isAttackOnCooldown = true;
+        yield return new WaitForSeconds(_cooldownTime);
+        _isAttackOnCooldown = false;
     }
 }
