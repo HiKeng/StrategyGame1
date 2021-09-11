a32e999c5f51a3cd74146c4a6a508be6780cb5e8
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-09-11 21:11:18 +0700

    VFX - Attack vfx & move trails.

---
+++ Assets/Scripts/VFX/Trails.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trails : MonoBehaviour
+{
+    Transform _transfromParent;
+
+    Vector3 _positionToMove;
+
+    private void Awake()
+    {
+        _transfromParent = transform.parent;
+    }
+
+    void Start()
+    {
+        transform.parent = null;
+    }
+
+    void Update()
+    {
+        _positionToMove = transform.position + (_transfromParent.transform.position - transform.position);
+
+        transform.position = _positionToMove;
+    }
+}
