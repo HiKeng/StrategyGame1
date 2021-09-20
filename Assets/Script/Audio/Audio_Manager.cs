4300d904bb15a6d6971dff786113e117d67519df
HiKeng <46863998+HiKeng@users.noreply.github.com>
2021-09-20 11:32:54 +0700

    Audio manager - Added basic functions.

---
+++ Assets/Script/Audio/Audio_Manager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class Audio_Manager : SingletonBase<Audio_Manager>
+{
+    [SerializeField] AudioMixer audioMixer;
+
+    public Sound[] SFXsounds;
+
+    AudioSource audioSource;
+
+    public AudioClip hitClip;
+
+    void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        _loadAudioSavedSetting();
+
+    }
+
+    public void playSFX(int index)
+    {
+        hitClip = SFXsounds[index].clip;
+        audioSource.PlayOneShot(hitClip);
+    }
+
+    public void _playThisSFX(AudioClip _sfxToPlay)
+    {
+        hitClip = _sfxToPlay;
+        audioSource.PlayOneShot(hitClip);
+    }
+
+    public void PlaySampleSFX(AudioClip _SampleSFX, bool _isPlayed)
+    {
+        if(_isPlayed)
+        {
+            audioSource.Play(0);
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void _loadAudioSavedSetting()
+    {
+        audioMixer.SetFloat("Master_Volume", Mathf.Log10(PlayerPrefs.GetFloat("Master_Volume")) * 20);
+        audioMixer.SetFloat("SFX_Volume", Mathf.Log10(PlayerPrefs.GetFloat("SFX_Volume")) * 20);
+        audioMixer.SetFloat("BGM_Volume", Mathf.Log10(PlayerPrefs.GetFloat("BGM_Volume")) * 20);
+    }
+}
---
+++ Assets/Script/Audio/PlaySFX.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+    public void playSFX(AudioClip _sfx)
+    {
+        Audio_Manager.Instance._playThisSFX(_sfx);
+    }
+}
